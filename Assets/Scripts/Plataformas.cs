7947abcace16a2c78d05b7b1a80f503e2f45ff86
CaroliCarol <a59626@alunos.ipb.pt>
2025-04-30 17:13:41 +0100

    Level 1

--- Assets/Scripts/Plataformas.cs
+++ Assets/Scripts/Plataformas.cs
@@ -4,32 +4,21 @@ using UnityEngine;
 
 public class Plataformas : MonoBehaviour
 {
-    public float jumpForce = 10;
     public bool isOnGround = true;
-    public bool gameOver = false;
-    private Rigidbody2D playerRb;
-    public float speed = 3.0f;
-    private float horizontal;
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody2D>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
-        {
-            playerRb.AddForce(Vector3.up*jumpForce, ForceMode2D.Impulse);
-            
-
-            //playerAnim.SetTrigger("Jump_trig");
-        }
+       
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Ground 1");
+        Debug.Log("Ground 2");
         if (collision.transform.tag == "Ground") 
         {    
             isOnGround = true;
