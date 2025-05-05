60c5c0ed47c0916f51e8bdf3a20ebd6dce714e13
CaroliCarol <a59626@alunos.ipb.pt>
2025-05-05 15:52:44 +0100

    Animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -45,7 +45,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up*jumpForce, ForceMode2D.Impulse);
             isOnGround = false;
-
+            //animator.SetFloat("NimboJump");
             //playerAnim.SetTrigger("Jump_trig");
         }
     }
